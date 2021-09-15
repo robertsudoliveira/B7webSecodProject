373e85a4ed949c778b2f7d6f8c772a95ca813570
Robert Oliveira <robertsudoliveira@gmail.com>
2021-09-15 11:17:08 -0300

    estado alterado

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 
 
+// Crie uma função e dê um nome para ela e que tenha 3(três) parâmetros, 
+//imprima seus valores no console utilizando o template literals (``).
 
-function frutas (banana, uva, laranja){
 
-console.log ( `${banana} ${uva} ${laranja}`)
-}
 
-frutas("banana", "uva", "laranja")
+
+// function frutas(banana, uva, laranja) {
+
+//     console.log(`${banana} ${uva} ${laranja}`)
+// }
+
+// frutas("banana", "uva", "laranja")
+
+
+// finalizado //
+
+
+
